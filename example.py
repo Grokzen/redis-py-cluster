b22a8f2614677bcb3ebfd1c46a739e5dffce92e9
Grokzen <Grokzen@gmail.com>
2014-02-27 16:25:56 +0100

    Change default port to 7000 to match the port used by Dockerfile

--- example.py
+++ example.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         "--port",
         help="port of a cluster member",
         type=int,
-        default=6379
+        default=7000
     )
     args = parser.parse_args()
 
