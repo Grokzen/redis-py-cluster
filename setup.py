46c0f03900c75ad500fe0985829ce98c3435691f
Grokzen <Grokzen@gmail.com>
2014-11-04 18:49:26 +0100

    Update version number to expected release number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('CHANGES') as f:
 
 setup(
     name="redis-py-cluster",
-    version="0.1.0",
+    version="0.2.0",
     description="Cluster library for redis 3.0.0 built on top of redis-py lib",
     long_description=alpha + '\n\n' + readme + '\n\n' + history,
     author="Johan Andersson",
