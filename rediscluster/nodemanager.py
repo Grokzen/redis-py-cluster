85790dc04d194ff163767ea94786154674d04bc5
etng <etng2004@gmail.com>
2015-11-18 12:12:31 +0800

    [FIX]dict method iteritems has changed to items in py3

--- rediscluster/nodemanager.py
+++ rediscluster/nodemanager.py
@@ -99,7 +99,7 @@ class NodeManager(object):
             'port',
             'decode_responses',
         )
-        connection_kwargs = {k: v for k, v in self.connection_kwargs.iteritems() if k in set(allowed_keys) - set(disabled_keys)}
+        connection_kwargs = {k: v for k, v in self.connection_kwargs.items() if k in set(allowed_keys) - set(disabled_keys)}
         return StrictRedis(host=host, port=port, decode_responses=decode_responses, **connection_kwargs)
 
     def initialize(self):
