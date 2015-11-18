e3c9ff5d98c31f405bf894732c462a2655641f4e
etng <etng2004@gmail.com>
2015-11-18 12:13:00 +0800

    add example of password protected cluster connection

---
+++ examples/basic_password_protected.py
@@ -0,0 +1,10 @@
+from rediscluster import StrictRedisCluster
+
+startup_nodes = [{"host": "127.0.0.1", "port": "7100"}]
+
+# Note: decode_responses must be set to True when used with python3
+rc = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True, password='password_is_protected')
+
+rc.set("foo", "bar")
+
+print(rc.get("foo"))
