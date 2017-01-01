64834dd873848f36a21e3c75f29ea090fed599fb
AngusP <a7cp3ar50n@gmail.com>
2017-01-01 12:43:47 +0000

    Usin redis-py's _compat `nativestr` in cluster_nodes and cluster_slots parsing funcs
    Also made minor changes to docs for clarity

--- rediscluster/utils.py
+++ rediscluster/utils.py
@@ -123,13 +123,10 @@ def nslookup(node_ip):
 def parse_cluster_slots(resp, **options):
     """
     """
-    if type(resp) is bytes:
-        resp = resp.decode('UTF-8')
-
     current_host = options.get('current_host', '')
 
     def fix_server(*args):
-        return (args[0] or current_host, args[1])
+        return (nativestr(args[0]) or current_host, args[1])
 
     slots = {}
     for slot in resp:
@@ -148,9 +145,7 @@ def parse_cluster_nodes(resp, **options):
     @see: http://redis.io/commands/cluster-nodes  # string
     @see: http://redis.io/commands/cluster-slaves # list of string
     """
-    if type(resp) is bytes:
-        resp = resp.decode('UTF-8')
-
+    resp = nativestr(resp)
     current_host = options.get('current_host', '')
 
     def parse_slots(s):
