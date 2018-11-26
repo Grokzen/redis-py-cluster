6f53500def8f04cdb677a9f921dd6f2f71c2539a
Grokzen <Grokzen@gmail.com>
2018-11-26 23:23:29 +0100

    Update __init__ to use new helper method to build VERSION attribute

--- rediscluster/__init__.py
+++ rediscluster/__init__.py
@@ -17,5 +17,15 @@ setattr(redis, "StrictClusterPipeline", StrictClusterPipeline)
 # Major, Minor, Fix version
 __version__ = (2, 0, 0)
 
+def int_or_str(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+__version__ = '2.0.0'
+VERSION = tuple(map(int_or_str, __version__.split('.')))
+
 if sys.version_info[0:3] == (3, 4, 0):
     raise RuntimeError("CRITICAL: rediscluster do not work with python 3.4.0. Please use 3.4.1 or higher.")
