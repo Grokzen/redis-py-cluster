0a6d283051b713f35f924028766adf5f7d6db62d
James Ward <james@notjam.es>
2019-02-25 00:36:14 -0500

    ensure OBJECT command is directed to correct node

--- rediscluster/client.py
+++ rediscluster/client.py
@@ -285,7 +285,10 @@ class StrictRedisCluster(StrictRedis):
                 raise RedisClusterException("{0} - all keys must map to the same key slot".format(command))
             return slots.pop()
 
-        key = args[1]
+        if command == 'OBJECT':
+            key = args[2]
+        else:
+            key = args[1]
 
         return self.connection_pool.nodes.keyslot(key)
 
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -85,8 +85,8 @@ class TestRedisCommands(object):
     def test_object(self, r):
         r['a'] = 'foo'
         assert isinstance(r.object('refcount', 'a'), int)
-        # assert isinstance(r.object('idletime', 'a'), int)
-        # assert r.object('encoding', 'a') in (b('raw'), b('embstr'))
+        assert isinstance(r.object('idletime', 'a'), int)
+        assert r.object('encoding', 'a') in (b('raw'), b('embstr'))
         assert r.object('idletime', 'invalid-key') is None
 
     def test_ping(self, r):
