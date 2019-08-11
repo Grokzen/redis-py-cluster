d5a1b703b08ea6c3dd32cd654f9f087618b1e26c
Grokzen <Grokzen@gmail.com>
2019-08-12 00:35:13 +0200

    Add python 3.7 to the compatible python version classifier list

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Environment :: Web Environment',
         'Operating System :: POSIX',
         'License :: OSI Approved :: MIT License',
