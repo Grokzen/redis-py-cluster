b1c7ffadd82103f234387dd198546dd08ef5af9e
wkuang <wkuang@tripadvisor.com>
2019-11-20 16:52:09 -0500

    typo

--- rediscluster/utils.py
+++ rediscluster/utils.py
@@ -101,7 +101,7 @@ def clusterdown_wrapper(func):
                 return func(*args, **kwargs)
             except ClusterDownError:
                 self = args[0]
-                self.refereh_table_asap = True
+                self.refresh_table_asap = True
                 # Try again with the new cluster setup. All other errors
                 # should be raised.
                 pass
