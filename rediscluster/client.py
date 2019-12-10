f9b34782bd3074f544ef4064b06dc46a67ee24be
wkuang <wkuang@tripadvisor.com>
2019-12-10 18:09:01 -0500

    revert whitespace change

--- rediscluster/client.py
+++ rediscluster/client.py
@@ -421,6 +421,7 @@ class RedisCluster(Redis):
                 raise
             except (ConnectionError, TimeoutError):
                 try_random_node = True
+
                 if ttl < self.RedisClusterRequestTTL / 2:
                     time.sleep(0.1)
             except ClusterDownError as e:
