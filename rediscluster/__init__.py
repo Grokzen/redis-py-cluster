57a1798739d7a4e3c9d58e887d96c928fbd12b5d
Grokzen <grokzen@gmail.com>
2020-05-15 23:44:02 +0200

    Restructure the __init__.py file to be more similar to redis-py where it enables easier importing of commonly used resources by removing the need to import subfiles and enable easier import like "from rediscluster import *" to get the most commonly used features.
    Removed the old monkeypatching where rediscluster objects was patches into the redis import name. This will be something breaking if someone is using this feature in their own code.

--- rediscluster/__init__.py
+++ rediscluster/__init__.py
@@ -3,19 +3,26 @@
 # python std lib
 import sys
 
-# Import shortcut
-from .client import RedisCluster
-from .pipeline import ClusterPipeline
-from .pubsub import ClusterPubSub
+# rediscluster imports
+from rediscluster.client import RedisCluster
+from rediscluster.connection import (
+    ClusterBlockingConnectionPool,
+    ClusterConnection,
+    ClusterConnectionPool,
+)
+from rediscluster.exceptions import (
+    RedisClusterException,
+    RedisClusterError,
+    ClusterDownException,
+    ClusterError,
+    ClusterCrossSlotError,
+    ClusterDownError,
+    AskError,
+    TryAgainError,
+    MovedError,
+    MasterDownError,
+)
 
-# Monkey patch RedisCluster class into redis for easy access
-import redis
-setattr(redis, "RedisCluster", RedisCluster)
-setattr(redis, "ClusterPubSub", ClusterPubSub)
-setattr(redis, "ClusterPipeline", ClusterPipeline)
-
-# Major, Minor, Fix version
-__version__ = (2, 1, 0)
 
 def int_or_str(value):
     try:
@@ -24,5 +31,23 @@ def int_or_str(value):
         return value
 
 
+# Major, Minor, Fix version
 __version__ = '2.1.0'
 VERSION = tuple(map(int_or_str, __version__.split('.')))
+
+__all__ = [
+    AskError,
+    ClusterBlockingConnectionPool,
+    ClusterConnection,
+    ClusterConnectionPool,
+    ClusterCrossSlotError,
+    ClusterDownError,
+    ClusterDownException,
+    ClusterError,
+    MasterDownError,
+    MovedError,
+    RedisCluster,
+    RedisClusterError,
+    RedisClusterException,
+    TryAgainError,
+]
