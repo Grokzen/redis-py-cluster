42a9c1a908a456c3a6f989a2ea47f3f77bfc5814
Grokzen <grokzen@gmail.com>
2020-05-18 17:44:51 +0200

    Make python 3.5 working again when building and installing this package

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         'redis>=3.0.0,<4.0.0'
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4, !=3.5",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4",
     extras_require={
         'hiredis': [
             "hiredis>=0.1.3",
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
