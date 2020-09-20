8146155e62b1ff08cbdf9c437860e35720133ba1
Grokzen <grokzen@gmail.com>
2020-09-20 11:50:19 +0200

    Set tag to 2.0.99rc2 for RC2 pypi upload

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(os.path.join('docs', 'release-notes.rst')) as f:
 
 setup(
     name="redis-py-cluster",
-    version="2.1.0",
+    version="2.0.99rc2",
     description="Library for communicating with Redis Clusters. Built on top of redis-py lib",
     long_description=readme + '\n\n' + history,
     long_description_content_type="text/markdown",
