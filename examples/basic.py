03414b06883029ea73e157dc9324c02e88c03641
Grokzen <Grokzen@gmail.com>
2021-04-18 22:03:30 +0200

    Update basic.py example with alternate way of using just host & port the regular redis-py way to connect to a cluster and not have to specify startup_nodes as a more complex object

--- examples/basic.py
+++ examples/basic.py
@@ -4,7 +4,14 @@ startup_nodes = [{"host": "127.0.0.1", "port": "7000"}]
 
 # Note: decode_responses must be set to True when used with python3
 rc = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
-
 rc.set("foo", "bar")
+print(rc.get("foo"))
 
+# Alternate simple mode of pointing to one startup node
+rc = RedisCluster(
+    host="127.0.0.1",
+    port=7000,
+    decode_responses=True,
+)
+rc.set("foo", "bar")
 print(rc.get("foo"))
